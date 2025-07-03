296d07e86d3a9a702d0de35c14908c0e35740aa0
mohdfardeen7007 <162273323+mohdfardeen7007@users.noreply.github.com>
2025-07-03 18:50:08 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,11 +6,11 @@ import fs from 'fs'
 import dotenv from 'dotenv'
 dotenv.config()
 
-const defaultOwner = '923444844060';
+const defaultOwner = '918127875972';
 
 
 // Check for the OWNERS environment variable; if not found, use the default
-const ownervb = process.env.OWNERS || process.env.OWNER_NUMBER || '' ; // put your number here
+const ownervb = process.env.OWNERS || process.env.OWNER_NUMBER || '918127875972' ; // put your number here
 
 const ownerlist = ownervb.split(';');
 
@@ -22,7 +22,7 @@ for (let i = 0; i < ownerlist.length; i++) {
 global.botname = process.env.BOTNAME || 'MEGA-AI';
 global.mods = []
 global.prems = []
-global.allowed = ['923444844060', '923051391007']
+global.allowed = ['918127875972']
 global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
 global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
 global.keysxteammm = [
